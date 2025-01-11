f62504bfb0e9b9a0b5fcb160889f887db855a070
SpeedaRJ <r.jutresa@gmail.com>
2025-01-11 01:12:49 +0100

    expanded restuarnat management

--- api/app.py
+++ api/app.py
@@ -1,33 +1,62 @@
 import os
+
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
-
-ADD_RESTAURANT="INSERT INTO restaurants (name, type) VALUES (%s,%s) RETURNING id"
-ADD_MENU_ITEM="INSERT INTO menu_items (name, price) VALUES (%s, %s) RETURNING id"
-ADD_MENU="INSERT INTO menus (restaurant_id, items) VALUES (%s, %s) RETURNING id"
+ADD_RESTAURANT = "INSERT INTO restaurants (name, type) VALUES (%s,%s) RETURNING id"
+ADD_MENU_ITEM = "INSERT INTO menu_items (name, price) VALUES (%s, %s) RETURNING id"
+ADD_MENU = "INSERT INTO menus (restaurant_id, items) VALUES (%s, %s) RETURNING id"
 load_dotenv()
 
 app = Flask(__name__)
+cors = CORS(app)
+
 
 @app.post("/add_restaurant")
+@cross_origin()
 def add_restaurant():
     data = request.get_json()
     res_name = data["name"]
     res_type = data["type"]
+    res_address = data["address"]
+    res_avg_time = data["average_time"]
+    res_price_range = data["price_range"]
 
     connection = psycopg2.connect(os.getenv("DATABASE_URL"))
 
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(ADD_RESTAURANT, (res_name, res_type,))
+            cursor.execute(
+                ADD_RESTAURANT,
+                (
+                    res_name,
+                    res_type,
+                    res_address,
+                    res_avg_time,
+                    res_price_range,
+                ),
+            )
             res_id = cursor.fetchone()[0]
 
     return jsonify({"restaurant_id": res_id})
 
 
+@app.get("/get_restaurants")
+@cross_origin()
+def get_restaurants():
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM restaurants")
+            restaurants = cursor.fetchall()
+
+    return jsonify({"resturant_list": restaurants})
+
+
 @app.post("/add_menu_item")
+@cross_origin()
 def add_menu_item():
     data = request.get_json()
     name = data["name"]
@@ -37,12 +66,31 @@ def add_menu_item():
 
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(ADD_MENU_ITEM, (name, price,))
+            cursor.execute(
+                ADD_MENU_ITEM,
+                (
+                    name,
+                    price,
+                ),
+            )
             menu_item_id = cursor.fetchone()[0]
 
     return jsonify({"menu_item_id": menu_item_id})
 
+
+@app.get("/get_menu_items")
+@cross_origin()
+def get_menu_items():
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM menu_items")
+            menu_items = cursor.fetchall()
+    return jsonify({"menu_items": menu_items})
+
+
 @app.post("/add_menu")
+@cross_origin()
 def add_menu():
     data = request.get_json()
     restaurant_id = data["restaurant_id"]
@@ -52,10 +100,31 @@ def add_menu():
 
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(ADD_MENU, (restaurant_id, items,))
+            cursor.execute(
+                ADD_MENU,
+                (
+                    restaurant_id,
+                    items,
+                ),
+            )
             menu_id = cursor.fetchone()[0]
 
     return jsonify({"menu_id": menu_id})
+
+
+@app.get("/get_menu_by_id")
+@cross_origin()
+def get_menu_by_id():
+    data = request.get_json()
+    restaurant_id = data["restaurant_id"]
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM menus WHERE id = %s", (restaurant_id,))
+            menu = cursor.fetchone()
+            menu_items = menu[2]
+    return jsonify({"menu_items": menu_items})
+
 
 if __name__ == "__main__":
     print("Starting app...")
